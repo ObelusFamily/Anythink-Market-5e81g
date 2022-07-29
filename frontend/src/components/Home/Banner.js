bf22c40fa18da0d88c8227a7d20977b3410669d3
Wayne del Corral Jr <wdelcorjr@gmail.com>
2022-07-29 01:00:16 -0500

    adds SearchBar component, begins prop work, incomplete commit

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Banner = () => {
   return (
@@ -7,7 +8,8 @@ const Banner = () => {
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
+          <span id="get-part">A Place to get</span>
+            <SearchBar />
           <span> the cool stuff.</span>
         </div>
       </div>
---
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,32 @@
+import { connect } from "react-redux";
+// import agent from "../../../src/agent"
+import { APPLY_TITLE_FILTER } from "../../constants/actionTypes";
+
+const mapStateToProps = (state) => ({
+    ...state.itemList,
+    title: '',
+  });
+  
+const mapDispatchToProps = (dispatch) => ({
+    onChange: (pager, payload) =>
+        dispatch({ type: APPLY_TITLE_FILTER, pager, payload }),
+});
+
+
+const SearchBar = (props) => {
+    const handleChange = (ev) => {
+        ev.preventDefault();
+    //   props.onChange("search", agent.Items.byTitle, agent.Items.byTitle());
+    // props.title: ev.target.value
+    console.log(`SearchBar value: ${ev.target.value}, props: ${props.items}`)
+    };
+
+    return (
+        <input
+        placeholder="What is it that you truly desire?"
+        onChange={handleChange}
+        />
+    );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchBar);
